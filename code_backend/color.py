68b6188f977ede58385089e643974f66f68e6eb3
icy <i@icys.top>
2022-06-04 22:44:04 +0800

    fix bugs

--- code_backend/color.py
+++ code_backend/color.py
@@ -16,7 +16,19 @@ def set_color(user, _color, time):
 
 @require_GET
 def get_color(request):
+    text = get_dict_from_request(request)
     user = session.get_user_by_request(request)
+    if not isinstance(user, models.User):
+        return user
+    if 'id' in text:
+        if not user.admin:
+            return response_json(status='Forbidden', message='Request is not allowed.')
+        try:
+            user = models.UserInfo.objects.filter(id=text['id'])
+        except models.UserInfo.DoesNotExist:
+            return response_json(status='NotFoundError', message='User not found.')
+        except models.UserInfo.MultipleObjectsReturned:
+            return response_json(status='SQLError', message='Multiple Objects Returned.')
     try:
         color = models.Color.objects.get(user=user)
     except models.Color.DoesNotExist:
